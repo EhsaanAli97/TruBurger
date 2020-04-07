c3fee346fe69d07838661279e791d2f7a29e6afe
Ehsaan Ali <38082320+EhsaanAli97@users.noreply.github.com>
2020-04-07 17:48:18 +0100

    Add files via upload
    Fixed Hamburger Menu bug

--- main.js
+++ main.js
@@ -26,21 +26,25 @@ function outsideClick(e) {
 }
 
 //Select element function
-const selectElement = function(element) {
+const selectElement = function (element) {
 	return document.querySelector(element);
 };
 
 let menuToggler = selectElement('.menu-toggle');
 let body = selectElement('body');
-let navToggler = selectElement('.item-toggle');
+// let navToggler = selectElement('.item-toggle');
 
-menuToggler.addEventListener('click', function() {
+menuToggler.addEventListener('click', function () {
 	body.classList.toggle('open');
 });
 
-navToggler.addEventListener('click', function() {
-	body.classList.toggle('open');
+$('a.nav-link').click(function () {
+	$('body').removeClass('open');
 });
+
+// navToggler.addEventListener('click', function () {
+// 	body.classList.toggle('open');
+// });
 
 // Scroll reveal
 window.sr = ScrollReveal();
@@ -49,26 +53,26 @@ sr.reveal('.animate-left', {
 	origin: 'left',
 	duration: 1000,
 	distance: '25rem',
-	delay: 300
+	delay: 300,
 });
 
 sr.reveal('.animate-right', {
 	origin: 'right',
 	duration: 1000,
 	distance: '25rem',
-	delay: 600
+	delay: 600,
 });
 
 sr.reveal('.animate-top', {
 	origin: 'top',
 	duration: 1000,
 	distance: '25rem',
-	delay: 600
+	delay: 600,
 });
 
 sr.reveal('.animate-bottom', {
 	origin: 'bottom',
 	duration: 1000,
 	distance: '25rem',
-	delay: 600
+	delay: 600,
 });
